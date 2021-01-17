f556cd5a44d14944e6a93a35ea2d904257885755
vagrant <vagrant@localhost.localdomain>
2021-01-17 09:49:50 +0000

    config complete

--- Gemfile
+++ Gemfile
@@ -49,6 +49,10 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'capistrano'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
 end
 
 group :test do
@@ -66,4 +70,5 @@ gem 'dotenv-rails'
 
 group :production, :staging do
   gem 'mysql2'
+  gem 'unicorn'
 end
---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.15.0"
+
+set :application, "expt"
+set :repo_url, "https://github.com/tarorin-work/expt.git"
+
+set :branch, 'master'
+
+set :deploy_to, '/var/www/expt_app'
+
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+set :keep_releases, 5
+
+set :rbenv_ruby, '2.5.7'
+
+set :log_level, :debug
+
+namespace :deploy do
+	desc 'Restart application'
+	task :restart do
+		invoke 'unicorn:restart'
+	end
+
+	desc 'Create database'
+	task :de_create do
+		on roles(:db) do |host|
+			with rails_env: fetch(:rails_env) do
+				within current_path do
+					sql = "CREATE DATABASE IF NOT EXIST expt_app_production"
+
+					execute "mysql --user=root --password=R|5f7.eP -e '#{sql}'"
+				end
+			end
+		end
+	end
+
+	agter :publishing, :restart
+
+	after :restart, :clear_cache do
+		on roles(:web), in :groups, limit: 3, wait: 10 do
+		end
+	end
+end
